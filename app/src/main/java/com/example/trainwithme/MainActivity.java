115de6d253d3db19434082d6273de4989872cc7e
OrtalGeller <ortal.geller001@gmail.com>
2019-07-01 20:06:01 +0300

    My Profile Layout

--- app/src/main/java/com/example/trainwithme/MainActivity.java
+++ app/src/main/java/com/example/trainwithme/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.trainwithme;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+
+import java.util.Calendar;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userDate;
+    DatePickerDialog datePickerDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userDate = findViewById(R.id.dateBirth);
+        userDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                final int year = calendar.get(Calendar.YEAR);
+                final int month = calendar.get(Calendar.MONTH);
+                final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        userDate.setText(day+"/"+(month+1)+"/"+year);
+                    }
+                },year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+
+
+
+
+
+
 
     }
 }
